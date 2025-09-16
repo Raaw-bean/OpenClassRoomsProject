ea7b9ef0009798c820c0b5b881e1739c16a80f92
raaw-bean <robinbinsalam@outlook.com>
2025-09-16 14:56:08 +0600

    added a new view function and url for about us

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def hello(request):
     return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About-Us</h1>  <p>We love merch!</p>')
